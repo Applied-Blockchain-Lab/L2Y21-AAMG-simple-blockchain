1ba737d832dce5e4a091f75633fb3cc49539e455
ahmedovv123 <ahmedovv123@gmail.com>
2021-12-10 11:17:53 +0200

    feat: Create transactions pool

---
+++ src/models/transactionsPool.js
@@ -0,0 +1,41 @@
+class TransactionsPool {
+
+  constructor() {
+    this.transactions = {}
+    this.maxTransactions = 30;
+  }
+
+  getAllPending() {
+    return this.transactions;
+  }
+
+  draw() {
+    var tx = this.transactions;
+    this.transactions = {};
+    return tx;
+  }
+
+  addTx(tx) {
+
+    if (this.transactions.length >= maxTransactions) {
+      console.log('Current block cant handle more transactions');
+    } else {
+      this.transactions[tx.hash] = tx;
+    } 
+    
+  }
+
+  removeTx() {
+    delete this.transactions[tx.hash];
+  }
+
+  addTxArr(txArr) {
+    txArr.forEach(tx => this.addTx(tx));
+  }
+
+  removeTxArr(txArr) {
+    txArr.forEach(tx => this.removeTx(tx));
+  }
+}
+
+module.exports = TransactionsPool;
