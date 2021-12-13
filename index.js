8ac2ad0f4c0d9ccd5f80c307637491e8b2027f28
ahmedovv123 <ahmedovv123@gmail.com>
2021-12-13 18:42:50 +0200

    feat: Create node instance and pass it to http server

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const HttpServer = require('./src/models/httpServer')
 const Blockchain = require('./src/models/blockchain')
 const TransactionsPool = require('./src/models/transactionsPool')
+const Node = require('./src/models/node');
 require('dotenv').config();
+
+const currentNodeUrl = process.argv[3];
+
+const httpPort = process.argv[2];
 
 let minerAddress = process.env.MINER_ADDRESS;
 
 var pendingTransactions = new TransactionsPool();
+var node = new Node(currentNodeUrl, "Just node");
+
 var blockchain = new Blockchain(pendingTransactions, minerAddress);
 
 
 HttpServer({
+  port: httpPort,
 	blockchain,
+  node,
 	pendingTransactions
 })
