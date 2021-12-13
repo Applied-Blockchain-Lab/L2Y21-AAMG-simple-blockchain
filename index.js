4be78de682301b3faa62252febd406907c762147
ahmedovv123 <ahmedovv123@gmail.com>
2021-12-13 10:25:04 +0200

    feat: Add new transaction route

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 1312; // temporary hardcoded port
+require('dotenv').config();
 
-const transactionsRoutes = require('./routes/transactions.js');
-const blocksRoutes = require('./routes/blocks.js');
+const transactionsRoutes = require('./src/routes/transactions.js');
+const blocksRoutes = require('./src/routes/blocks.js');
+const blockchainRoutes = require('./src/routes/blockchain');
 
 app.use("/transactions", transactionsRoutes);
 app.use("/blocks", blocksRoutes);
+app.use("/blockchain", blockchainRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
